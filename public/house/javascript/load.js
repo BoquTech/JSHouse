4cedbfd49c958892f6cc40631af3aa6e4ba03402
rickyall <783814127@qq.com>
2014-08-30 11:14:13 +0800

    V1.0.8

---
+++ public/house/javascript/load.js
@@ -0,0 +1,86 @@
+/*轮播图片加载*/
+$(function() {
+    $('#slider').nivoSlider();
+});
+
+
+/*浮动栏加载*/
+$(document).ready(function(){
+	var  top1=$("#pro-introduce").offset().top;
+	var  top2=$("#pro-data").offset().top;
+	var  top3=$("#pro-invest").offset().top;
+	var  top4=$("#pro-map").offset().top;
+	function Add_Data1()
+	{
+		var scrolla=$(window).scrollTop();
+		var cha1=parseInt(top1)-parseInt( scrolla); 
+		var cha2=parseInt(top2)-parseInt( scrolla); 
+		var cha3=parseInt(top3)-parseInt( scrolla); 
+		var cha4=parseInt(top4)-parseInt( scrolla); 
+		if(cha2>200){
+			$("#tab1").removeClass("tab-item2").addClass("tab-item1"); 
+			$("#tab2").removeClass("tab-item1").addClass("tab-item2"); 
+			$("#tab3").removeClass("tab-item1").addClass("tab-item2"); 
+			$("#tab4").removeClass("tab-item1").addClass("tab-item2"); 
+		
+		}
+		if(cha2<=200&&cha3>200){
+			$("#tab1").removeClass("tab-item1").addClass("tab-item2"); 
+			$("#tab2").removeClass("tab-item2").addClass("tab-item1"); 
+			$("#tab3").removeClass("tab-item1").addClass("tab-item2"); 
+			$("#tab4").removeClass("tab-item1").addClass("tab-item2");
+		}
+        if(cha3<=200&&cha4>200){
+        	$("#tab1").removeClass("tab-item1").addClass("tab-item2"); 
+			$("#tab2").removeClass("tab-item1").addClass("tab-item2"); 
+			$("#tab3").removeClass("tab-item2").addClass("tab-item1"); 
+			$("#tab4").removeClass("tab-item1").addClass("tab-item2");
+
+        }
+         if(cha4<=200){
+        	$("#tab1").removeClass("tab-item1").addClass("tab-item2"); 
+			$("#tab2").removeClass("tab-item1").addClass("tab-item2"); 
+			$("#tab3").removeClass("tab-item1").addClass("tab-item2"); 
+			$("#tab4").removeClass("tab-item2").addClass("tab-item1");
+
+        }
+
+	}
+	$(window).scroll(Add_Data1); 
+	}); 
+
+
+
+
+$(document).ready(function(){ 
+var scrolla=$(window).scrollTop();
+var loaded = true; 
+var top = $("#fix").offset().top; 
+function Add_Data2() 
+{ 
+var cha=parseInt(top)-parseInt(scrolla); 
+if(loaded && cha<=0) 
+{ 
+$("#fix").removeClass("fix2").addClass("fix1"); 
+loaded=false; 
+} 
+if(!loaded && cha>0) 
+{ 
+$("#fix").removeClass("fix1").addClass("fix2"); 
+loaded=true; 
+} 
+} 
+$(window).scroll(Add_Data2); 
+}); 
+
+
+$(document).ready(function () {
+
+            $.goup({
+                trigger: 100,
+                bottomOffset: 180,
+                locationOffset: 100,
+                titleAsText: true
+            });
+        });
+
