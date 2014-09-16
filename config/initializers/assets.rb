2c429b3d33e73cc753d2dd0ddb7297ccde86a5cb
rickyall <783814127@qq.com>
2014-09-16 14:00:04 +0800

    v1.1.4

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,10 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( jquery-1.4.3.min.js )
 Rails.application.config.assets.precompile += %w( jquery.nivo.slider.pack.js )
 Rails.application.config.assets.precompile += %w( nivo-slider.css )
+Rails.application.config.assets.precompile += %w( fix_bar.js )
+Rails.application.config.assets.precompile += %w( top.js )
+
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
