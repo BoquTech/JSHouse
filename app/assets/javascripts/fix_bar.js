a628dcef7eab8feb1777063130a671e610ee4ae8
rickyall <783814127@qq.com>
2014-09-19 13:47:16 +0800

    V1.1.6
    样式调整完毕

--- app/assets/javascripts/fix_bar.js
+++ app/assets/javascripts/fix_bar.js
@@ -25,41 +25,28 @@ $(document).ready(function(){
 	var  top1=$("#pro-introduce").offset().top;
 	var  top2=$("#pro-data").offset().top;
 	var  top3=$("#pro-invest").offset().top;
-	var  top4=$("#pro-map").offset().top;
 	function Add_Data()
 	{
 		var scrolla=$(window).scrollTop();
 		var cha1=parseInt(top1)-parseInt( scrolla); 
 		var cha2=parseInt(top2)-parseInt( scrolla); 
 		var cha3=parseInt(top3)-parseInt( scrolla); 
-		var cha4=parseInt(top4)-parseInt( scrolla); 
 		if(cha2>200){
 			$("#tab1").removeClass("tab-item2").addClass("tab-item1"); 
 			$("#tab2").removeClass("tab-item1").addClass("tab-item2"); 
-			$("#tab3").removeClass("tab-item1").addClass("tab-item2"); 
-			$("#tab4").removeClass("tab-item1").addClass("tab-item2"); 
-		
+			$("#tab3").removeClass("tab-item1").addClass("tab-item2");  		
 		}
 		if(cha2<=200&&cha3>200){
 			$("#tab1").removeClass("tab-item1").addClass("tab-item2"); 
 			$("#tab2").removeClass("tab-item2").addClass("tab-item1"); 
 			$("#tab3").removeClass("tab-item1").addClass("tab-item2"); 
-			$("#tab4").removeClass("tab-item1").addClass("tab-item2");
 		}
-        if(cha3<=200&&cha4>200){
+        if(cha3<=200){
         	$("#tab1").removeClass("tab-item1").addClass("tab-item2"); 
 			$("#tab2").removeClass("tab-item1").addClass("tab-item2"); 
 			$("#tab3").removeClass("tab-item2").addClass("tab-item1"); 
-			$("#tab4").removeClass("tab-item1").addClass("tab-item2");
-
         }
-         if(cha4<=200){
-        	$("#tab1").removeClass("tab-item1").addClass("tab-item2"); 
-			$("#tab2").removeClass("tab-item1").addClass("tab-item2"); 
-			$("#tab3").removeClass("tab-item1").addClass("tab-item2"); 
-			$("#tab4").removeClass("tab-item2").addClass("tab-item1");
 
-        }
 
 	}
 	$(window).scroll(Add_Data); 
