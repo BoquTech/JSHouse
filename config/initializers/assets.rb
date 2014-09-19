307ea38f014e25c6b36da2a146340f48baf963db
rickyall <783814127@qq.com>
2014-09-19 10:59:02 +0800

    Revert 7a50ab8..196985b
    This rolls back to commit 7a50ab86273fb713a7a2b3a7a31009df646fc4b5.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,14 +3,10 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( jquery-1.4.3.min.js )
-Rails.application.config.assets.precompile += %w( cssreset.css )
 Rails.application.config.assets.precompile += %w( jquery.nivo.slider.pack.js )
 Rails.application.config.assets.precompile += %w( nivo-slider.css )
 Rails.application.config.assets.precompile += %w( fix_bar.js )
 Rails.application.config.assets.precompile += %w( top.js )
-Rails.application.config.assets.precompile += %w( publicset.css )
-Rails.application.config.assets.precompile += %w( housepage.css )
-Rails.application.config.assets.precompile += %w( cssreset.css )
 
 
 # Precompile additional assets.
