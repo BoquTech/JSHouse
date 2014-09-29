894e98e04c1514ce17b392c6ad5c2ac2b1a3ad01
rickyall <783814127@qq.com>
2014-09-29 16:33:32 +0800

    v1.1.9.5
    1.修正了图片大小的问题。
    2.实现了翻译功能。

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -5,6 +5,14 @@ class PropertiesController < ApplicationController
 
   # GET /properties
   # GET /properties.json
+  def properties
+    require 'digest/sha1'
+    property_token='B0quTech'
+    array=[property_token,]
+
+  end
+
+
   def index
     @properties = Property.all
   
@@ -13,10 +21,40 @@ class PropertiesController < ApplicationController
   # GET /properties/1
   # GET /properties/1.json
   def show
-   @code=@property.description
-   @city=@property.city
-   @address=Address.where(city: @city).first
 
+    @city=@property.city
+    @address=Address.where(city: @city).first
+   
+   require 'uri'
+
+   require 'net/http'
+  
+   @building_name2=@property.building_name
+   @description2=@property.description
+   @address2=@property.address
+   @features2=@property.features
+
+   @building_name =  URI.encode(@building_name2)
+   @description=  URI.encode(@description2)
+   @features=  URI.encode(@features2)
+   @address=  URI.encode(@address2)
+   uri = URI("http://openapi.baidu.com/public/2.0/bmt/translate?client_id=wczI7xUBqYDdxOWIQsnoCAi5&q=#{@building_name}%0A#{@description}%0A#{@features}%0A#{@address}&from=auto&to=auto
+")
+
+  Net::HTTP.start(uri.host, uri.port,
+  :use_ssl => uri.scheme == 'https') do |http|
+  request = Net::HTTP::Get.new uri
+  response = http.request request
+  res=JSON.parse response.body
+
+  @building_name1=res["trans_result"][0]['dst']
+  @description1=res["trans_result"][1]['dst'].html_safe
+  @features1=res["trans_result"][2]['dst'].html_safe
+  @address1=res["trans_result"][3]['dst'].html_safe
+
+end
+
+   
   end
 
   # GET /properties/new
@@ -78,4 +116,5 @@ class PropertiesController < ApplicationController
     def property_params
       params.require(:property).permit(:title, :description, :address, :bedrooms, :bathrooms, :family_rooms, :reception_rooms, :latitude, :longitude, :features, :agent_id, :agent_name, :date_available, :building_name, :district, :indoor_size, :saleable_area, :buy, :rent, :refid, :building_age, :outdoor_size, :float, :inclusive_display, :inclusive_mfee, :inclusive_govr, :address_display, :street, :view, :layout, :floor, :uuid, :user_id, :data_source, :enabled,:tag_list, item_images_attributes: [:id, :_destroy], item_images: [])
     end
+   
 end
