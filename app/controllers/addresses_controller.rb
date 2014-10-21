d5c7f5d006ec986f1bd1bf736c6f1abc05277782
rickyall <783814127@qq.com>
2014-10-21 21:56:29 +0800

    dadf

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -8,8 +8,8 @@ class AddressesController < ApplicationController
 
   def index
     @addresses = Address.all
-    @australia=Address.where(country: '澳大利亚')
-    @england=Address.where(country: '英国')
+    @australia=Address.where(country: 'Australia')
+    @england=Address.where(country: 'England')
   end
 
   # GET /addresses/1
