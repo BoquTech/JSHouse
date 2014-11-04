1059ac291a850d1a9f8727c490742183428cbcde
rickyall <783814127@qq.com>
2014-11-04 16:24:25 +0800

    v1.9.9.7
    修复了相关Bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require jquery-1.4.3.min
+//= require jquery.nivo.slider.pack
 //= require_tree
-//= require cocoon
 $(function() {
       $('#slider').nivoSlider();
   });
